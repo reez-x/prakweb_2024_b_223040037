2bb56511a61c36de71ced5c56cd8274acff646b2
reez 45 <rizkyabdurrahman455@gmail.com>
2024-10-08 11:54:21 +0700

    Video 5 - View

--- MVC/app/controllers/About.php
+++ MVC/app/controllers/About.php
@@ -1,10 +1,19 @@
 <?php
 
-class About{
-    public function index($nama = Rizky, $pekerjaan = "3D artist"){
-        echo "Halo, nama saya $nama, saya adalah seorang $pekerjaan";
+class About extends Controller{
+    public function index($nama = "Rizky", $pekerjaan = "3D artist", $umur = 18){
+        $data['nama'] = $nama;
+        $data['pekerjaan'] = $pekerjaan;
+        $data['umur'] = $umur;
+        $data['judul'] = 'About Me';
+        $this->view('templates/header', $data);
+        $this->view('about/index', $data);
+        $this->view('templates/footer', $data);
     }
     public function page(){
-        echo 'About/page';
+        $data['judul'] = 'Pages';
+        $this->view ('templates/header', $data);
+        $this->view ('about/page');
+        $this->view ('templates/footer');
     }
 }
--- MVC/app/controllers/Home.php
+++ MVC/app/controllers/Home.php
@@ -1,7 +1,10 @@
 <?php
 
-class Home {
+class Home extends Controller{
     public function index(){
-        echo 'home/index';
+        $data['judul'] = 'Home';
+        $this->view('templates/header');
+        $this->view('home/index');
+        $this->view('templates/footer');
     }
 }
