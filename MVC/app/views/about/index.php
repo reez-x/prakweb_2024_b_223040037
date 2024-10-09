4617f559551e6a3fc310b54862ea8543daf6f815
reez 45 <rizkyabdurrahman455@gmail.com>
2024-10-09 12:53:28 +0700

    Video 6 - Assets

--- MVC/app/views/about/index.php
+++ MVC/app/views/about/index.php
@@ -1,3 +1,5 @@
-
+<div class="container">
+    <img src="<?= BASEURL; ?>/img/profile.jpeg" alt="Reez" width="200" class="rounded-circle shadow">
     <h1>About me</h1>
     <p>Halo, nama saya <?= $data['nama']; ?>, umur saya <?= $data['umur']; ?> tahun, saya adalah seorang <?= $data['pekerjaan']; ?></p>
+</div>
