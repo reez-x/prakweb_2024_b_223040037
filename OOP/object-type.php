cbf2e0605fbeaecc67aebb4a3611748acb05bfce
reez 45 <rizkyabdurrahman455@gmail.com>
2024-10-13 22:01:20 +0700

    Video 5 - Object Type

--- OOP/object-type.php
+++ OOP/object-type.php
@@ -17,6 +17,14 @@ class Produk{
             return "$this->penulis, $this->penerbit";
         }
 
+
+}
+
+class CetakInfoProduk{
+    public function cetak(Produk $produk){
+        $str = "{$produk->judul} | {$produk->getLabel()} (Rp. {$produk->harga})";
+        return $str;
+    }
 }
 
 // $produk1 = new Produk();
@@ -30,10 +38,10 @@ class Produk{
 
 $produk1 = new Produk("Naruto", "Masashi Kishimoto", "Shonen Jump", 30000);
 $produk2 = new Produk("Uncharted", "Neil Druckman", "Sony Computer", 250000);
-$produk3 = new Produk("Uncharted", "", "Sony Computer", 250000);
 
 echo "Komik : " . $produk1->getLabel();
 echo "<br>";
 echo "Game : " . $produk2->getLabel();
 echo "<br>";
-echo "Game : " . $produk3->getLabel();
+$infoProduk1 = new CetakInfoProduk();
+echo $infoProduk1->cetak($produk1);
