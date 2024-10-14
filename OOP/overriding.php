3d4f87333e7d33844f7f6253e7bcd2ede112ed32
reez 45 <rizkyabdurrahman455@gmail.com>
2024-10-14 14:24:20 +0700

    Video 8 - Overriding

--- OOP/overriding.php
+++ OOP/overriding.php
@@ -5,17 +5,14 @@ class Produk{
         $penulis,
         $penerbit,
         $harga,
-        $jmlHalaman,
         $waktuMain;
 
 
-        public function __construct($judul = "judul", $penulis  = "penulis", $penerbit = "penerbit", $harga  = 0, $jmlHalaman = 0, $waktuMain = 0){
+        public function __construct($judul = "judul", $penulis  = "penulis", $penerbit = "penerbit", $harga  = 0){
             $this->judul = $judul;
             $this->penulis = $penulis;
             $this->penerbit = $penerbit;
             $this->harga = $harga;
-            $this->jmlHalaman = $jmlHalaman;
-            $this->waktuMain = $waktuMain;
         }
 
         public function getLabel(){
@@ -32,6 +29,13 @@ class Produk{
 }
 
 class Komik extends Produk{
+    public $jmlHalaman;
+
+    public function __construct($judul = "judul", $penulis  = "penulis", $penerbit = "penerbit", $harga  = 0, $jmlHalaman = 0){
+        parent::__construct( $judul, $penulis, $penerbit, $harga);
+        $this->jmlHalaman = $jmlHalaman;
+    }
+
     public function getInfoProduk(){
         $str = "Komik : " . parent::getInfoProduk() . " - {$this->jmlHalaman} Halaman.";
         return $str;
@@ -39,8 +43,15 @@ class Komik extends Produk{
 } 
 
 class Game extends Produk{
+    public $waktuMain;
+
+    public function __construct( $judul = "judul", $penulis  = "penulis", $penerbit = "penerbit", $harga  = 0, $waktuMain = 0  ){
+        parent::__construct( $judul, $penulis, $penerbit = "penerbit", $harga  = 0, $waktuMain = 0 );
+        $this->waktuMain = $waktuMain;
+    }
+
     public function getInfoProduk(){
-        $str = "Game : {$this->judul} | {$this->getLabel()} (Rp. {$this->harga}) ~ {$this->waktuMain} Jam.";
+        $str = "Game : " . parent::getInfoProduk() . " ~ {$this->waktuMain} Jam.";
         return $str;
     }
 }
@@ -61,9 +72,8 @@ class CetakInfoProduk{
 // $produk2->tambahProperty = "lol";
 // var_dump($produk2->judul);
 
-$produk1 = new Komik("Naruto", "Masashi Kishimoto", "Shonen Jump", 30000, 100 , 0);
-$produk2 = new Game("Uncharted", "Neil Druckman", "Sony Computer", 250000, 0, 50);
-$produk2 = new Produk("Uncharted", "Neil Druckman", "Sony Computer", 250000, 0, 50);
+$produk1 = new Komik("Naruto", "Masashi Kishimoto", "Shonen Jump", 30000, 100);
+$produk2 = new Game("Uncharted", "Neil Druckman", "Sony Computer", 250000,50);
 
 // Komik : Masashi Kishimoto, Shonen Jump
 // Game : Neil Druckman, Sony Computer
