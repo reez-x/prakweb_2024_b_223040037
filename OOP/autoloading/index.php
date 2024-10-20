8a6f23a61622f1de8c1c133e4b89039de4207fc6
reez 45 <rizkyabdurrahman455@gmail.com>
2024-10-21 06:36:21 +0700

    Video 17 - Namespace

--- OOP/autoloading/index.php
+++ OOP/autoloading/index.php
@@ -1,16 +1,23 @@
 <?php
 require_once 'App/init.php';
-$produk1 = new Komik("Naruto", "Masashi Kishimoto", "Shonen Jump", 30000, 100);
-$produk2 = new Game("Uncharted", "Neil Druckman", "Sony Computer", 250000,50);
+// $produk1 = new Komik("Naruto", "Masashi Kishimoto", "Shonen Jump", 30000, 100);
+// $produk2 = new Game("Uncharted", "Neil Druckman", "Sony Computer", 250000,50);
 
-// Komik : Masashi Kishimoto, Shonen Jump
-// Game : Neil Druckman, Sony Computer
-// Naruto | Masashi Kishimoto, Shonen Jump (Rp. 30000)
+// // Komik : Masashi Kishimoto, Shonen Jump
+// // Game : Neil Druckman, Sony Computer
+// // Naruto | Masashi Kishimoto, Shonen Jump (Rp. 30000)
 
-$cetakProduk = new CetakInfoProduk();
-$cetakProduk->tambahProduk($produk1);
-$cetakProduk->tambahProduk($produk2);
-echo $cetakProduk->cetak();
+// $cetakProduk = new CetakInfoProduk();
+// $cetakProduk->tambahProduk($produk1);
+// $cetakProduk->tambahProduk($produk2);
+// echo $cetakProduk->cetak();
 
-echo "<hr>";
-new Coba();
+// echo "<hr>";
+// new Coba();
+
+use App\Service\User as ServiceUser;
+use App\Produk\User as ProdukUser;
+
+new ServiceUser();
+echo "<br>";
+new ProdukUser();
